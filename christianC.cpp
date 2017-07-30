553138bbd68a1b260d13be6b17dd90f194827708
Christian <chrisreycap@gmail.com>
2017-07-30 16:08:25 -0700

    removed sound. Simran's job

--- christianC.cpp
+++ christianC.cpp
@@ -164,8 +164,6 @@ void renderHeart4();
 void christianInit();
 
 // Everything about sound
-extern void csound(const char*);
-void shootSound();
 
 //the "main function of my file"
 void renderChristianSprites(int);
@@ -380,8 +378,6 @@ void shootParticle()
     if(gl.oneOffShootFlag == true) {
         printf("Shoot\n");
         makeParticle(); 
-        //shootSound();
-        csound("./sound/Shoot.wav");
         gl.oneOffShootFlag = false;
     }
 }
@@ -940,65 +936,5 @@ void renderChristianSprites(int charSelect)
     renderHeart3();
     renderHeart4();
     particlePhysics(charSelect);
-}
-
-void shootSound(){
-    #ifdef USE_OPENAL_SOUND
-    cout <<"@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@SHOOT SOUNDDDDDD\n";
-    alutInit(0, NULL);
-    if (alGetError() != AL_NO_ERROR) {
-        printf("ERROR: alutInit()\n");
-        return;
-    }
-    //Clear error state.
-    alGetError();
-
-    //
-    //Setup the listener.
-    //Forward and up vectors are used.
-    float vec[6] = {0.0f,0.0f,1.0f, 0.0f,1.0f,0.0f};
-    alListener3f(AL_POSITION, 0.0f, 0.0f, 0.0f);
-    alListenerfv(AL_ORIENTATION, vec);
-    alListenerf(AL_GAIN, 1.0f);
-    
-
-    //Buffer holds the sound information.
-    ALuint alBuffer;
-    alBuffer = alutCreateBufferFromFile("./sound/Shoot.wav");
-    //
-    //Source refers to the sound.
-    ALuint alSource;
-    //Generate a source, and store it in a buffer.
-    alGenSources(1, &alSource);
-    alSourcei(alSource, AL_BUFFER, alBuffer);
-    //Set volume and pitch to normal, no looping of sound.
-    alSourcef(alSource, AL_GAIN, 1.0f);
-    alSourcef(alSource, AL_PITCH, 1.0f);
-    alSourcei(alSource, AL_LOOPING, AL_FALSE);
-    if (alGetError() != AL_NO_ERROR) {
-        printf("ERROR: setting source\n");
-        return;
-    }
-    alSourcePlay(alSource);
-    //Cleanup.
-    //First delete the source.
-    alDeleteSources(1, &alSource);
-    //Delete the buffer.
-    alDeleteBuffers(1, &alBuffer);
-    //Close out OpenAL itself.
-    //Get active context.
-/*
-    ALCcontext *Context = alcGetCurrentContext();
-    //Get device for active context.
-    ALCdevice *Device = alcGetContextsDevice(Context);
-    //Disable context.
-    alcMakeContextCurrent(NULL);
-    //Release context(s).
-    alcDestroyContext(Context);
-    //Close device.
-    alcCloseDevice(Device);
-  */
-    #endif
-    return;
 }
 
